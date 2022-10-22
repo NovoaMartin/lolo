7ed195d11ee3dc3bff88776d6a8cf3829fcfb5b3
Martin Novoa <martinnovoar2@gmail.com>
2022-10-22 12:37:00 -0300

    Rotacion segun la direccion

--- src/main/java/Utils/Direccion.java
+++ src/main/java/Utils/Direccion.java
@@ -5,4 +5,13 @@ public class Direccion {
     public static final int DOWN = 1;
     public static final int LEFT = 2;
     public static final int RIGHT = 3;
+
+    public static int getRotation(int direccion) {
+        return switch (direccion) {
+            case DOWN -> 0;
+            case LEFT -> 90;
+            case RIGHT -> 270;
+            default -> 180;
+        };
+    }
 }
