b78ee241ad83b0dca504cfd294ed5bdbd7e638ab
Martin Novoa <martinnovoar2@gmail.com>
2022-10-22 12:37:39 -0300

    Animacion de roca

--- src/main/java/enviroment/MovableRock.java
+++ src/main/java/enviroment/MovableRock.java
@@ -3,21 +3,23 @@ package enviroment;
 import Utils.Celda;
 import Utils.Direccion;
 import character.Character;
+import javafx.animation.TranslateTransition;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import javafx.util.Duration;
 import lolo.Mapa;
 
 public class MovableRock extends Enviroment {
-    Shape shape;
+    Shape image;
 
     public MovableRock(Celda pos) {
         super(pos);
-        shape = new Rectangle(50, 50);
-        shape.setTranslateY(2.5 + pos.y * 50);
-        shape.setTranslateX(2.5 + pos.x * 50);
-        shape.setFill(Color.DARKGREEN);
+        image = new Rectangle(50, 50);
+        image.setTranslateY(2.5 + pos.y * 50);
+        image.setTranslateX(2.5 + pos.x * 50);
+        image.setFill(Color.DARKGREEN);
     }
 
     private int orientacion = Direccion.RIGHT;
@@ -29,21 +31,28 @@ public class MovableRock extends Enviroment {
 
     @Override
     public void interactWith(Character character, int direccion, Mapa mapa) {
+        Celda inicial = this.getPos();
         boolean moved = this.tryMove(direccion, mapa);
         if (moved) {
-            character.tryMove(direccion);
+            character.setPos(inicial);
         }
     }
 
     @Override
     public Node getRender() {
-        return shape;
+        return image;
     }
 
     @Override
     public void setPos(Celda pos) {
         super.setPos(pos);
-        shape.setTranslateY(2.5 + pos.y * 50);
-        shape.setTranslateX(2.5 + pos.x * 50);
+        TranslateTransition animacion = new TranslateTransition(Duration.millis(500), image);
+        animacion.setToX(2.5 + pos.x * 50);
+        animacion.setToY(2.5 + pos.y * 50);
+        animacion.play();
+        animacion.setOnFinished(e -> {
+            image.setTranslateX(2.5 + pos.x * 50);
+            image.setTranslateY(2.5 + pos.y * 50);
+        });
     }
 }
